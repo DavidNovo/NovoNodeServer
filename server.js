100779db2c606ab7df30505df7b30555ff0c5871
DavidNovo <david.novogrodsky@gmail.com>
2015-10-22 23:10:27 -0500

    CHAP01: added ability to read query strings that are part of url.

--- server.js
+++ server.js
@@ -4,14 +4,16 @@
 
 var http = require('http');
 var path = require('path');
+var url = require('url');
+
 
 // defining our routes, for the router function
 var pages = [
-    {route: '/',  output: 'Whoohoo!'},
-    {route: '/about', output: 'A simple routing example with Node'},
+    {id: '1', route: '/',  output: 'Whoohoo!'},
+    {id: '2', route: '/about', output: 'A simple routing example with Node'},
     // anonymous function
-    {route: '/another page', output:  function() {return 'Here\'s ' + this.route; } },
-    {route: '/about/me', output: 'My name is David'}
+    {id: '3', route: '/another page', output:  function() {return 'Here\'s ' + this.route; } },
+    {id: '4', route: '/about/me', output: 'My name is David'}
 ];
 
 http.createServer(function(request,response) {
@@ -20,14 +22,29 @@ http.createServer(function(request,response) {
     // when using just decodeURI, add '/' to the routing
     var lookup = decodeURI(request.url);
 
-    // for each possible route in the array check against  incoming lookup
-    pages.forEach(function(page) {
-        if (page.route === lookup) {
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            // respone can be a string or  function call, a noun or a verb
-            response.end(typeof page.output === 'function' ? page.output() : page.output);
+    // check if there is a query string
+    var id = url.parse(decodeURI(request.url), true).query.id;
+
+    if (id ) {
+        // for each possible route in the array check against  incoming query id
+        pages.forEach(function(page) {
+            if (page.id === id) {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                // respone can be a string or  function call, a noun or a verb
+                response.end(typeof page.output === 'function' ? page.output() : page.output);
+            }
+        });
+    } else {
+        // for each possible route in the array check against  incoming lookup
+        pages.forEach(function(page) {
+            if (page.route === lookup) {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                // respone can be a string or  function call, a noun or a verb
+                response.end(typeof page.output === 'function' ? page.output() : page.output);
+            }
+        });
         }
-    });
+
     if (!response.finished) {
         response.writeHead(404);
         response.end('Page Not Found!');
