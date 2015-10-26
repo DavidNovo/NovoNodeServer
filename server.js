38380ff5e368f567306950abff6f4e389d3e3814
DavidNovo <david.novogrodsky@gmail.com>
2015-10-26 00:59:32 -0500

    CHAP01: now server can handle static content. Need to fix 404 message.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var http = require('http');
 var path = require('path');
 var url = require('url');
 
+// this module is needed to serve static files
+var fs = require('fs');
+
 
 // defining our routes, for the router function
 var pages = [
@@ -14,16 +17,48 @@ var pages = [
     // anonymous function
     {id: '3', route: '/another page', output:  function() {return 'Here\'s ' + this.route; } },
     {id: '4', route: '/about/me', output: 'My name is David'}
-];
+    ];
+
+// adding mime types so the server knows what to do with them
+var mimeTypes = {
+    '.js' : 'text/javascript',
+    '.html' : 'text/html',
+    '.css' : 'text/css'
+};
+
 
 http.createServer(function(request,response) {
     // first get the part of the URI after host name
     // decode the URI so spaces look like %20
     // when using just decodeURI, add '/' to the routing
-    var lookup = decodeURI(request.url);
+    var lookup = decodeURI(request.url) || 'index.html';
+    f = 'content/' + lookup;
+
 
     // check if there is a query string
     var id = url.parse(decodeURI(request.url), true).query.id;
+
+    // checking to serve static
+    // this is assynchronus, uses callback function
+    fs.exists(f, function(exists) {
+        console.log(exists ? lookup + " is a static file " : lookup + " is not a static file");
+        if (exists) {
+            fs.readFile(f,  function(err, data) {
+                if (err) {
+                    response.writeHead(500);
+                    response.end('Server error!! ');
+                    return;  }
+                var headers = {'Content-type' : mimeTypes[path.extname(lookup)] };
+                response.writeHead(200, headers);
+                response.end(data);
+            });
+            return;
+        }
+        response.writeHead(404);
+        response.end('Static Page Not Found!');
+        return;
+    });
+
 
     if (id ) {
         // for each possible route in the array check against  incoming query id
@@ -45,8 +80,8 @@ http.createServer(function(request,response) {
         });
         }
 
-    if (!response.finished) {
-        response.writeHead(404);
-        response.end('Page Not Found!');
-    }
+ //   if (!response.finished) {
+ //       response.writeHead(404);
+ //       response.end('Page Not Found! Fall through error...');
+ //   }
 }).listen(8080);
