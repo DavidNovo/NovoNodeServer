6693bab61da41d2c27ffe7e92623a69e4c5d7c71
DavidNovo <david.novogrodsky@gmail.com>
2015-11-05 22:11:21 -0600

    CHAP01: beautifying the javascript.

--- server.js
+++ server.js
@@ -11,19 +11,34 @@ var fs = require('fs');
 
 
 // defining our routes, for the router function
-var pages = [
-    {id: '1', route: '/',  output: 'Whoohoo!'},
-    {id: '2', route: '/about', output: 'A simple routing example with Node'},
+var pages = [{
+        id: '1',
+        route: '/',
+        output: 'Whoohoo!'
+    }, {
+        id: '2',
+        route: '/about',
+        output: 'A simple routing example with Node'
+    },
     // anonymous function
-    {id: '3', route: '/another page', output:  function() {return 'Here\'s ' + this.route; } },
-    {id: '4', route: '/about/me', output: 'My name is David'}
-    ];
+    {
+        id: '3',
+        route: '/another page',
+        output: function() {
+            return 'Here\'s ' + this.route;
+        }
+    }, {
+        id: '4',
+        route: '/about/me',
+        output: 'My name is David'
+    }
+];
 
 // adding mime types so the server knows what to do with them
 var mimeTypes = {
-    '.js' : 'text/javascript',
-    '.html' : 'text/html',
-    '.css' : 'text/css'
+    '.js': 'text/javascript',
+    '.html': 'text/html',
+    '.css': 'text/css'
 };
 
 // make cache for static files
@@ -33,24 +48,27 @@ var cache = {};
 // function to get  and put stuff into cache
 function cacheAndDeliver(filepath, cb) {
     // adding in some stuff for time checking
-    fs.stat(filepath,  function(err, stats) {
+    fs.stat(filepath, function(err, stats) {
         var lastChanged = Date.parse(stats.ctime);
         isUpdated = (cache[filepath]) && lastChanged > cache[filepath].timestamp;
-            if ( !cache[filepath] || isUpdated ) {
-                fs.readFile(filepath, function(err, data) {
-                    if (!err){
-                        cache[filepath] = {content : data, timestamp: Date.now()};  // store a Unix timestamp
-                    }
-                    cb(err, data);
-                });
-                return;
-            }
-            //console.log( 'loading ' + filepath + ' from cache'  );
-            cb( null,  cache[filepath].content );
+        if (!cache[filepath] || isUpdated) {
+            fs.readFile(filepath, function(err, data) {
+                if (!err) {
+                    cache[filepath] = {
+                        content: data,
+                        timestamp: Date.now()
+                    }; // store a Unix timestamp
+                }
+                cb(err, data);
+            });
+            return;
+        }
+        //console.log( 'loading ' + filepath + ' from cache'  );
+        cb(null, cache[filepath].content);
     }); // end of fs.stat
 } // end of cacheAndDeliver
 
-http.createServer(function(request,response) {
+http.createServer(function(request, response) {
     // first get the part of the URI after host name
     // decode the URI so spaces look like %20
     // when using just decodeURI, add '/' to the routing
@@ -64,14 +82,17 @@ http.createServer(function(request,response) {
     // checking to serve static
     // this is assynchronus, uses callback function
     fs.exists(filepath, function(exists) {
-       // console.log(exists ? lookup + " is a static file " : lookup + " is not a static file");
+        // console.log(exists ? lookup + " is a static file " : lookup + " is not a static file");
         if (exists) {
-            cacheAndDeliver(filepath,  function(err, data) {
+            cacheAndDeliver(filepath, function(err, data) {
                 if (err) {
                     response.writeHead(500);
                     response.end('Server error!! ');
-                    return;  }
-                var headers = {'Content-type' : mimeTypes[path.extname(lookup)] };
+                    return;
+                }
+                var headers = {
+                    'Content-type': mimeTypes[path.extname(lookup)]
+                };
                 response.writeHead(200, headers);
                 response.end(data);
             });
@@ -83,17 +104,21 @@ http.createServer(function(request,response) {
     });
 
 
-   // for each possible route in the array check against  incoming lookup
+    // for each possible route in the array check against  incoming lookup
     pages.forEach(function(page) {
         if (page.route === lookup) {
-            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
             // respone can be a string or  function call, a noun or a verb
             response.end(typeof page.output === 'function' ? page.output() : page.output);
-        } else if (id ) {
+        } else if (id) {
             // for each possible route in the array check against  incoming query id
             pages.forEach(function(page) {
                 if (page.id === id) {
-                    response.writeHead(200, {'Content-Type': 'text/html'});
+                    response.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
                     // respone can be a string or  function call, a noun or a verb
                     response.end(typeof page.output === 'function' ? page.output() : page.output);
                 }
--- test/serverTest.js
+++ test/serverTest.js
@@ -2,102 +2,101 @@
 //  creation of tests
 //
 
-var assert  =  require('assert');
-var http      =  require('http');
-var server = require( '../server.js' );
+var assert = require('assert');
+var http = require('http');
+var server = require('../server.js');
 
 
 describe('basic tests of server ', function() {
 
-        before(function() {
-          // runs before all tests in this block
+    before(function() {
+        // runs before all tests in this block
+    });
+
+    after(function() {
+        // runs after all tests in this block
+    });
+
+    beforeEach(function() {
+        // runs before each test in this block
+    });
+
+    afterEach(function() {
+        // runs after each test in this block
+    });
+
+
+    // test cases
+    //
+    // look at the output the describe() function defines main headers to group tests
+    //  the it() function is outputed under the tests in the describe() function
+    //  thing of this as an outline
+    //  basic tests of server
+    //          contacting...
+    //          the path ->...
+    //          /about should....
+    //          send this URI
+    //                requesting /xxx....
+
+    describe(' send this URI to the server /xxx should return an HTTP status of 404', function() {
+        it(' requesting /xxx from the server should return 404', function(done) {
+            http.get('http://localhost:8080/xxx', function(res) {
+                assert.equal(404, res.statusCode);
+                done();
+            });
         });
+    });
 
-        after(function() {
-          // runs after all tests in this block
+
+    it('contacting the server root URL should return 200', function(done) {
+        http.get('http://localhost:8080/', function(res) {
+            assert.equal(200, res.statusCode);
+            done();
         });
+    });
 
-        beforeEach(function() {
-          // runs before each test in this block
+    // Node uses the 'on()'  function to register event handlers
+    // the first parameter defines when the handler function is called
+    //  the second part is the handler function
+    it(' the path -> / should output Whoohoo', function(done) {
+        http.get('http://localhost:8080/', function(res) {
+            assert.equal(200, res.statusCode);
+
+            var data = '';
+            res.on('data', function(chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function() {
+                assert.equal('Whoohoo!', data);
+                done();
+            });
         });
+    });
 
-        afterEach(function() {
-          // runs after each test in this block
+    // Node uses the 'on()'  function to register event handlers
+    // the first parameter defines when the handler function is called
+    //  the second part is the handler function
+    it(' /about should output  -> A simple routing example with Node', function(done) {
+        http.get('http://localhost:8080/about', function(res) {
+            assert.equal(200, res.statusCode);
+
+            var data = '';
+            res.on('data', function(chunk) {
+                data += chunk;
+            });
+
+            res.on('end', function() {
+                assert.equal('A simple routing example with Node', data);
+                done();
+            });
         });
+    });
 
-
-      // test cases
-      //
-      // look at the output the describe() function defines main headers to group tests
-      //  the it() function is outputed under the tests in the describe() function
-      //  thing of this as an outline
-      //  basic tests of server
-      //          contacting...
-      //          the path ->...
-      //          /about should....
-      //          send this URI
-      //                requesting /xxx....
-
-      describe(' send this URI to the server /xxx should return an HTTP status of 404', function () {
-            it(' requesting /xxx from the server should return 404', function (done) {
-                  http.get('http://localhost:8080/xxx', function (res) {
-                        assert.equal(404, res.statusCode);
-                        done();
-                  });
-            });
-      });
-
-
-      it('contacting the server root URL should return 200', function (done) {
-            http.get('http://localhost:8080/', function (res) {
-                  assert.equal(200, res.statusCode);
-                  done();
-            });
-      });
-
-      // Node uses the 'on()'  function to register event handlers
-      // the first parameter defines when the handler function is called
-      //  the second part is the handler function
-            it(' the path -> / should output Whoohoo', function (done) {
-                  http.get('http://localhost:8080/', function (res) {
-                        assert.equal(200, res.statusCode);
-
-                        var data = '';
-                        res.on('data', function (chunk) {
-                              data += chunk;
-                        });
-
-                        res.on('end', function () {
-                              assert.equal('Whoohoo!', data );
-                              done();
-                        });
-                  });
-            });
-
-       // Node uses the 'on()'  function to register event handlers
-      // the first parameter defines when the handler function is called
-      //  the second part is the handler function
-            it(' /about should output  -> A simple routing example with Node', function (done) {
-                  http.get('http://localhost:8080/about', function (res) {
-                        assert.equal(200, res.statusCode);
-
-                        var data = '';
-                        res.on('data', function (chunk) {
-                              data += chunk;
-                        });
-
-                        res.on('end', function () {
-                              assert.equal('A simple routing example with Node', data );
-                              done();
-                        });
-                  });
-            });
-
-      it('contacting the server root URL/about should return 200', function (done) {
-            http.get('http://localhost:8080/about', function (res) {
-                  assert.equal(200, res.statusCode);
-                  done();
-            });
-      });
+    it('contacting the server root URL/about should return 200', function(done) {
+        http.get('http://localhost:8080/about', function(res) {
+            assert.equal(200, res.statusCode);
+            done();
+        });
+    });
 }); // end of basic tests
-
